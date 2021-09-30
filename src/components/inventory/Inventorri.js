c2336d7368776be8e364553bc5e5b7859632f9f4
sejan2 <sejansiju2020@gmail.com>
2021-10-01 00:01:16 +0600

    hello

---
+++ src/components/inventory/Inventorri.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventorri = () => {
+    return (
+        <div>
+            <h2>This is Inventory</h2>
+        </div>
+    );
+};
+
+export default Inventorri;
